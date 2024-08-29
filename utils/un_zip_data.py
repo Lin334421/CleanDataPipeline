c31b8d3c22ef33f9e65490629c673ab439e4c3c9
malin <linruo9936@163.com>
2024-08-29 10:02:36 +0800

    添加解压数据工具包

---
+++ utils/un_zip_data.py
@@ -0,0 +1,32 @@
+import gzip
+import calendar
+import time
+from loguru import logger
+logger.add('un_zip.log')
+year_list = [2024]
+month_list = [5]
+data_parent_path = '/opt/mission_area/gha_data/'
+def un_gzip(gz_filename):
+    try:
+        filename = gz_filename.replace('.gz', '')
+        g_file = gzip.GzipFile(gz_filename)
+        open(f'{data_parent_path}{filename}', 'wb+').write(g_file.read())
+        g_file.close()
+        logger.info(f"successful unzip {gz_filename}")
+    except:
+        logger.error(f"unzip {gz_filename} failed")
+
+
+for year in year_list:
+    count = 0
+    for month in month_list:
+        day_count = calendar.monthrange(year, month)[1]
+        if month < 10:
+            month = '0' + str(month)
+        for i in range(1, day_count + 1):
+            if i < 10:
+                i = '0' + str(i)
+            for j in range(24):
+                un_gzip(f"{year}-{month}-{i}-{j}.json.gz")
+                count+=1
+
